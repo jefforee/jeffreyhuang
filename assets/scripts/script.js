f957460d1f9738345ae85c281eeaf35bd14d6286
Jeffrey Huang <jeff0827@berkeley.edu>
2022-01-02 19:11:32 -0800

    Navbar changes color after moving out of dark screen

---
+++ assets/scripts/script.js
@@ -0,0 +1,17 @@
+window.addEventListener("scroll", function(event) {
+    var scroll = this.scrollY;
+    if(scroll > 1000) {
+        let navOptions = document.getElementsByClassName("nav_options");
+        for (let i = 0; navOptions.length; i++)
+        navOptions[i].setAttribute("style", "color: black");
+    }
+})
+
+window.addEventListener("scroll", function(event) {
+    var scroll = this.scrollY;
+    if(scroll < 1000) {
+        let navOptions = document.getElementsByClassName("nav_options");
+        for (let i = 0; navOptions.length; i++)
+        navOptions[i].setAttribute("style", "color: white");
+    }
+})
